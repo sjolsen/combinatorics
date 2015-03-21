9764949098c72594f1e28482cfaadda00d62ce87
Stuart Olsen <so1132@txstate.edu>
2015-03-20 22:40:13 -0500

    2 in Part II

--- code/derangements.lisp
+++ code/derangements.lisp
@@ -1,3 +1,29 @@
+(defun factorial (n)
+  (declare (type (integer 0) n)
+           (optimize (speed 3)
+                     (debug 0)))
+  (labels ((f (n acc)
+             (if (zerop n)
+                 acc
+                 (f (1- n) (* n acc)))))
+    (f n 1)))
+
+(defun binom (r k)
+  (declare (type real    r)
+           (type integer k)
+           (optimize (speed 3)
+                     (debug 0)))
+  (cond ((< k 0) 0)
+        ((= k 0) 1)
+        (t (/ (loop
+                 with acc = 1
+                 for i from r above (- r k)
+                 do (setf acc (* acc i))
+                 finally (return acc))))))
+
+(defun mulnom (above &rest below)
+  (apply #'/ (mapcar #'factorial (list* above below))))
+
 (defun derangements (n)
   (labels ((-1^ (n)
              (if (evenp n)
